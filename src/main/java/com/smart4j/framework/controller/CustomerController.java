a76584dadcfc48830fa18de2d98acd44980d4923
niems <mingod@qq.com>
2017-10-24 09:50:09 +0800

    no message

--- src/main/java/com/smart4j/framework/controller/CustomerController.java
+++ src/main/java/com/smart4j/framework/controller/CustomerController.java
@@ -3,12 +3,14 @@ package com.smart4j.framework.controller;
 import com.smart4j.framework.annotation.Action;
 import com.smart4j.framework.annotation.Controller;
 import com.smart4j.framework.annotation.Inject;
+import com.smart4j.framework.bean.Data;
 import com.smart4j.framework.bean.Param;
 import com.smart4j.framework.bean.View;
 import com.smart4j.framework.model.Customer;
 import com.smart4j.framework.service.CustomerService;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sam on 2017/10/24.
@@ -22,5 +24,40 @@ public class CustomerController {
     public View index(Param param) {
         List<Customer> customerList = customerService.getCustomerList();
         return new View("customer.jsp").addModel("customerList", customerList);
+    }
+    @Action("get:/customer_show")
+    public View show(Param param) {
+        long id = param.getLong("id");
+        Customer customer = customerService.getCustomer(id);
+        return new View("customer_show.jsp").addModel("customer", customer);
+    }
+    @Action("get:/customer_create")
+    public View create(Param param) {
+        return new View("customer_create.jsp");
+    }
+    @Action("post:/customer_create")
+    public Data createSubmit(Param param) {
+        Map<String, Object> fieldMap = param.getMap();
+        boolean result = customerService.createCustomer(fieldMap);
+        return new Data(result);
+    }
+    @Action("get:/customer_edit")
+    public View edit(Param param) {
+        long id = param.getLong("id");
+        Customer customer = customerService.getCustomer(id);
+        return new View("customer_edit.jsp").addModel("customer", customer);
+    }
+    @Action("put:/customer_edit")
+    public Data editSubmit(Param param) {
+        long id = param.getLong("id");
+        Map<String, Object> fieldMap = param.getMap();
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        return new Data(result);
+    }
+    @Action("delete:/customer_edit")
+    public Data delete(Param param) {
+        long id = param.getLong("id");
+        boolean result = customerService.deleteCustomer(id);
+        return new Data(result);
     }
 }
